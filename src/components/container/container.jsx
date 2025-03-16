c2101f49cf29d1dedb7cafb7c9437dbb1340a38c
AMADOU <147598248+amadoudiop04@users.noreply.github.com>
2025-03-16 15:32:04 +0100

    update cv

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -12,7 +12,7 @@ const container = () => {
     <p className='text-1'> hello, I'm </p>
     <p className='text-2'> Amadou Diop </p>
     <p className='text-3'> Junior Developer </p>
-    <a href="https://drive.google.com/file/d/1CBhamk1q0hhxNW9-tZcNCN8U0xMsInqB/view?usp=sharing"> <button className='btn-1'>Download CV</button></a>
+    <a href="https://drive.google.com/file/d/1Aotkt7wFh8s48WUGcrwD_WTfiIr-1L0p/view?usp=sharing"> <button className='btn-1'>Download CV</button></a>
     <div>
       <a href="https://github.com/amadoudiop04"><img className='logo' src={image} alt="logo" /></a>
     </div>
