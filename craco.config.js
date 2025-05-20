bf3f2927c98aa25601185d6204abdfe495932222
AMADOU <147598248+amadoudiop04@users.noreply.github.com>
2025-05-20 13:12:21 +0200

    update portfolio design building

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,21 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 module.exports = {
-    webpack: {
-      configure: (webpackConfig) => {
-        const cssRule = webpackConfig.module.rules.find(rule =>
-          rule.test && rule.test.toString().includes('.css')
-        );
-        if (cssRule) {
-          cssRule.use = cssRule.use.filter(loader => !loader.loader.includes('postcss-loader'));
-        }
-        return webpackConfig;
-      },
+  webpack: {
+    configure: (webpackConfig) => {
+      const cssRule = webpackConfig.module.rules.find(rule =>
+        rule.test && rule.test.toString().includes('.css')
+      );
+      if (cssRule) {
+        cssRule.use = cssRule.use.filter(loader => !loader.loader.includes('postcss-loader'));
+      }
+      webpackConfig.plugins.push(
+        new ESLintPlugin({
+          extensions: ['js', 'jsx'],
+        })
+      );
+
+      return webpackConfig;
     },
-  };
-  
+  },
+};
