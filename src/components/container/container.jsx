91d372085ea3ed1611f77d2dcb2c0d7238ac8b17
AMADOU <147598248+amadoudiop04@users.noreply.github.com>
2025-08-05 12:17:43 +0200

    update cv

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -97,7 +97,7 @@ const Container = () => {
 
         <div className="profile-actions">
           <a
-            href="https://drive.google.com/file/d/1PtXpX5vawn6dtfzhuZZRjpdXGFiAO4qs/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1OqPHjMid_ROLKq8nWItK0ZGfiXnt-vTY/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="cv-button-wrapper"
